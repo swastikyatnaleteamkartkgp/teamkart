45a4d2a4a34859f2227f02d543c3bae0463ce513
swastikyatnaleteamkartkgp <swastikyatnale.teamkartkgp@gmail.com>
2026-04-09 00:29:06 +0530

    Update smtp_script.py

--- smtp_script.py
+++ smtp_script.py
@@ -13,7 +13,7 @@ EMAIL = "swastikyatnale.teamkartkgp@gmail.com"
 PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 # Put in the correct csv file name 
-data = pd.read_csv('test_csv.csv', encoding = 'utf-8')
+data = pd.read_csv('test.csv', encoding = 'utf-8')
 data.columns = data.columns.str.strip()
 
 # Definitions
@@ -92,8 +92,6 @@ HTML_BODY = """
 <p>Thank you for your time and consideration.</p>
 </div>
 </body>
-
-
 """
 
 HTML_TAIL="""
