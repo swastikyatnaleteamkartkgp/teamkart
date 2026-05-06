7f60a1cbffb81833d0dbb0ffa61a3c5e69a0f174
swastikyatnaleteamkartkgp <swastikyatnale.teamkartkgp@gmail.com>
2026-05-06 09:52:28 +0530

    Update smtp_script.py

--- smtp_script.py
+++ smtp_script.py
@@ -26,7 +26,7 @@ YOUR_ROLE_TK = "Mechanical Subsystem Trainee"
 YOUR_CONTACT = "+91 9890699650"
 YOUR_LINKED_IN = "https://www.linkedin.com/in/swastikyatnale/"
 YOUR_FACEBOOK = "https://www.facebook.com/TeamKART/"
-CC_EMAILS = ["prajitpradeep.teamkartkgp@gmail.com"]
+CC_EMAILS = ["prajitpradeep.teamkartkgp@gmail.com", "mohamed.teamkartkgp@gmail.com"]
 SUBJECT = "Follow-up: Potential Partnership with TeamKART, IIT Kharagpur"
 HTML_HEAD = """
 <!DOCTYPE html>
